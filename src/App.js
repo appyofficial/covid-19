e870933026bcd561755b33c07f68cf14eface586
Appy Sharma <appy70890@gmail.com>
2020-04-15 08:43:52 +0200

    countries updated

--- src/App.js
+++ src/App.js
@@ -5,18 +5,27 @@ import { fetchData } from "./api";
 
 class App extends React.Component {
   state = {
-    data: {}
+    data: {},
+    country: ""
   };
+
   async componentDidMount() {
     const fetchedData = await fetchData();
     this.setState({ data: fetchedData });
   }
+
+  handleCountryChange = async country => {
+    //fetch data
+    const fetchedData = await fetchData(country);
+    this.setState({ data: fetchedData, country: country });
+  };
+
   render() {
     const { data } = this.state;
     return (
       <div className={styles.App}>
         <Cards data={data} />
-        <CountryPicker />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Chart />
       </div>
     );
--- src/api/index.js
+++ src/api/index.js
@@ -2,11 +2,17 @@ import axios from "axios";
 
 const url = "https://covid19.mathdro.id/api";
 
-export const fetchData = async () => {
+export const fetchData = async country => {
+  let changebleUrl = url;
+
+  if (country) {
+    changebleUrl = `${url}/countries/${country}`;
+  }
+
   try {
     const {
       data: { confirmed, deaths, recovered, lastUpdate }
-    } = await axios.get(url);
+    } = await axios.get(changebleUrl);
 
     const modifiedData = {
       confirmed,
